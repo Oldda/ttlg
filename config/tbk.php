6d68bd0a02a6b9e2153208d7fd9ceff4df9099a3
renpengchao <597291343@qq.com>
2019-06-14 18:24:23 +0800

    修改配置文件

--- config/tbk.php
+++ config/tbk.php
@@ -3,5 +3,5 @@ return [
     'key' => env('TBK_APP_KEY', '27587538'),
     'secret' => env('TBK_APP_SECRET', 'bfabdcaea6e7c05578a696fe6a30d416'),
     'cat' => env('TBK_PRODUCT_CAT', ''),
-	'adzone_id' => env('ADZONE_ID','554000343');
+	'adzone_id' => env('ADZONE_ID','554000343')
 ];
