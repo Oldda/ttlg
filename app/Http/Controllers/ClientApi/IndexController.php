dfe42e1ef3ff6116dfcd1e87c16d0d8d37070728
renpengchao <597291343@qq.com>
2019-06-18 19:14:11 +0800

    '提供淘宝客接口，未完待续'

--- app/Http/Controllers/ClientApi/IndexController.php
+++ app/Http/Controllers/ClientApi/IndexController.php
@@ -81,7 +81,7 @@ class IndexController extends Controller
         $input = [
             'limit' => request('limit',$this->limit),
             'page'  => request('page',$this->page),
-			'sort'  => 'total_sales_desc',
+//			'sort'  => 'total_sales_desc',
 			'has_coupon' => 'true',
 			'is_tmall' => 'true',
 			'need_free_shipment' => 'true',
