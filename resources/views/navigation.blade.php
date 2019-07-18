9aba0b0b040f3893e3461d3c53d692df7e8debf3
renpengchao <597291343@qq.com>
2019-07-18 18:29:16 +0800

    修改图片域名

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <div class="content">
-		<img src="http://psxc4vqan.bkt.clouddn.com/131560592483_.pic_hd_big.jpg" class="img-responsive" alt="领券指南" style="width:100%;height:100%;"> 
+		<img src="http://img.wukehui.cn/131560592483_.pic_hd_big.jpg" class="img-responsive" alt="领券指南" style="width:100%;height:100%;"> 
     </div>
 </body>
 </html>
